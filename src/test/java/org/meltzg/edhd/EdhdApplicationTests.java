bd2147f207c47a47020e0e8f660c53557e793c89
Greg Meltzer <gregory.m.meltzer@gmail.com>
2018-02-03 10:34:47 -0500

    going through security tutorial

---
+++ src/test/java/org/meltzg/edhd/EdhdApplicationTests.java
@@ -0,0 +1,16 @@
+package org.meltzg.edhd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EdhdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
