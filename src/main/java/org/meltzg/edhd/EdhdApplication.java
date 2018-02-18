d622809a36c28282cea02cc7cad861b35d2770c1
Greg Meltzer <gregory.m.meltzer@gmail.com>
2018-02-18 17:46:19 -0500

    getting login working

--- src/main/java/org/meltzg/edhd/EdhdApplication.java
+++ src/main/java/org/meltzg/edhd/EdhdApplication.java
@@ -1,12 +1,35 @@
 package org.meltzg.edhd;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class EdhdApplication {
+@RestController
+public class EdhdApplication extends WebSecurityConfigurerAdapter {
+	
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.antMatcher("/**")
+			.authorizeRequests()
+				.antMatchers("/", "/login**", "/bower_components/**", "/src/**")
+				.permitAll()
+			.anyRequest()
+				.authenticated();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdhdApplication.class, args);
---
+++ src/main/java/org/meltzg/edhd/security/SecurityController.java
@@ -0,0 +1,14 @@
+package org.meltzg.edhd.security;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+public class SecurityController {
+//	@RequestMapping("/user")
+//	public Principal user(Principal principal) {
+//		return principal;
+//	}
+}
