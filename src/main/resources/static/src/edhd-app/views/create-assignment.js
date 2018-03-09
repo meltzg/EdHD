4669ef38b638112388e84e73ad3dd0a8bcdaa63e
Greg Meltzer <gregory.m.meltzer@gmail.com>
2018-03-08 22:48:51 -0500

    layout change

--- src/main/resources/static/src/edhd-app/views/create-assignment.js
+++ src/main/resources/static/src/edhd-app/views/create-assignment.js
@@ -4,8 +4,16 @@ class CreateAssignment extends Polymer.Element {
         return {
             primaryConfig: {
                 type: Object
+            },
+            selectedConf: {
+                type: Number,
+                value: 0,
+                observer: 'doop'
             }
         };
+    }
+    doop() {
+        console.log(this.selectedConf)
     }
 }
 customElements.define(CreateAssignment.is, CreateAssignment);
